b9ecf9bb4c9e748a96a4a0f6826690b97063f9c7
Be.Lev <68658819+be-lev@users.noreply.github.com>
2021-10-24 20:07:42 +0300

    static site as a blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+//if we want to put something in all the routes we put it here, fpr example header/Nav
+import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className='container'>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
